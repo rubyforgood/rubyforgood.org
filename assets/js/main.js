0b372f435ac24d11587d0500cd7d02e7112e5586
FionaDL <fionadlapham@gmail.com>
2021-02-17 12:20:55 -0500

    Update owl-carousel to have previous and next arrows for supporters

--- assets/js/main.js
+++ assets/js/main.js
@@ -210,8 +210,11 @@
 
   $(".clients-active").owlCarousel({
     loop: true,
-    nav: false,
-    autoplay: true,
+    nav: true,
+    autoplay: false,
+    margin:10,
+    nav:true,
+    navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
     responsive: {
       0: {
         items: 2
