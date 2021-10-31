a4c46cfc8499e7d7a3b0dc4ad69915a1c972f7b3
Meg Gutshall <meghan.gutshall@gmail.com>
2021-10-30 21:19:03 -0400

    Adjust configs

--- Gemfile
+++ Gemfile
@@ -23,8 +23,9 @@ gem "github-pages", group: :jekyll_plugins
 group :jekyll_plugins do
   gem "jekyll-feed", "~> 0.6"
   gem "jekyll-paginate-v2"
-  gem 'jekyll-tagging'
-  gem 'jekyll-archives'
+  gem "jekyll-tagging"
+  gem "jekyll-archives"
+  gem "jekyll-postcss"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+const jekyllEnvironment = process.env.JEKYLL_ENV || 'development'
+
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('postcss-custom-properties')({}),
+    require('postcss-nested'),
+    require('autoprefixer'),
+    ...(jekyllEnvironment != 'development'
+      ? [
+          require('@fullhuman/postcss-purgecss')({
+            content: ['!(_site|node_modules)/**/*.+(html|js|md)', '*.html'],
+            whitelistPatternsChildren: [/highlight/],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || []
+          }),
+          require('cssnano')({ preset: 'default' })
+        ]
+      : [])
+  ]
+}
